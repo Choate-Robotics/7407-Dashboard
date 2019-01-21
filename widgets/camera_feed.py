8959cf78e11ef8c9c06307fa08cb2a8783cc90ab
Ruocheng Wang <yrrejkk@gmail.com>
2019-01-21 17:20:10 -0500

    Camera Config Sliders Finalized

--- widgets/camera_feed.py
+++ widgets/camera_feed.py
@@ -1,25 +1,30 @@
 from __future__ import annotations
 
+import json
 import socket
+import struct
+import sys
 import threading
+import time
+import traceback
 from collections import deque
 from io import BytesIO
-import time
 
-from PIL import Image
-
-from PySide2.QtCore import QObject, Signal,Qt
+from PIL import Image,ImageFile
+from PySide2.QtCore import QObject, Qt, Signal
 from PySide2.QtGui import QImage, QPixmap
-from PySide2.QtWidgets import QHBoxLayout, QLabel, QSizePolicy, QSpacerItem, QVBoxLayout, QWidget, QTabWidget, QFrame, QGridLayout,QSlider
-import struct
+from PySide2.QtWidgets import (QFrame, QGridLayout, QHBoxLayout, QLabel, QPushButton, QSizePolicy, QSlider, QTabWidget, QVBoxLayout, QWidget)
 
 IMAGE_BUFFER_SIZE = 1024
 REMOTE_IP_ADDR = '10.9.5.107'  # '10.74.7.4'
 FRAME_START_IDENTIFIER = b'\n_\x92\xc3\x9c>\xbe\xfe\xc1\x98'
 
-DEFAULT_IMAGE_QUALITY=25
-DEFAULT_RESOLUTION=240
+DEFAULT_IMAGE_QUALITY = 25
+DEFAULT_RESOLUTION = 240
 
+DEBUG = True
+
+ImageFile.LOAD_TRUNCATED_IMAGES=True
 class Signals(QObject):
     imageReady = Signal(QImage)
     updateStatus = Signal(int, int, int)  # total,server process time, client time
@@ -76,10 +81,27 @@ class RateTracker:
 
 
 class TrafficMonitor(metaclass=SingletonMeta):
-    cam0 = RateTracker(5)
-    cam1 = RateTracker(5)
-    cam2 = RateTracker(5)
-    cam3 = RateTracker(5)
+    cam0 = RateTracker(2)
+    cam1 = RateTracker(2)
+    cam2 = RateTracker(2)
+    cam3 = RateTracker(2)
+    
+    def __init__(self, n_camera: int):
+        self._n_camera = n_camera
+    
+    @property
+    def total(self):
+        v = 0
+        for i in range(self._n_camera):
+            v += getattr(self, 'cam%d' % i)
+        return v
+
+
+class FrameRateMonitor(metaclass=SingletonMeta):  # not really a subclass, but the similar functionality
+    cam0 = RateTracker(1)
+    cam1 = RateTracker(1)
+    cam2 = RateTracker(1)
+    cam3 = RateTracker(1)
     
     def __init__(self, n_camera: int):
         self._n_camera = n_camera
@@ -91,12 +113,11 @@ class TrafficMonitor(metaclass=SingletonMeta):
             v += getattr(self, 'cam%d' % i)
         return v / self._n_camera
 
-
-class FrameRateMonitor(metaclass=SingletonMeta):  # not really a subclass, but the similar functionality
-    cam0 = RateTracker(1)
-    cam1 = RateTracker(1)
-    cam2 = RateTracker(1)
-    cam3 = RateTracker(1)
+class FrameDropMonitor(metaclass=SingletonMeta):
+    cam0 = RateTracker(10)
+    cam1 = RateTracker(10)
+    cam2 = RateTracker(10)
+    cam3 = RateTracker(10)
     
     def __init__(self, n_camera: int):
         self._n_camera = n_camera
@@ -116,28 +137,27 @@ class FeedReceiver(threading.Thread):
         self.camera_feed_widget = camera_feed
         self.signals = Signals()
         self.width = 960
-        self.signals.frameResize.connect(self.updateFrameSize)
-        
-
     
     def run(self):
+        self.signals.frameResize.connect(self.updateFrameSize)
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
             sock.bind(('0.0.0.0', 5801 + self.camera_id))
-            sock.sendto(FRAME_START_IDENTIFIER, (REMOTE_IP_ADDR, 5800))
+            #sock.sendto(FRAME_START_IDENTIFIER, (REMOTE_IP_ADDR, 5800))
             while True:
                 try:
-                    header = sock.recv(32)
+                    header = sock.recv(40)
                     while header[:10] != FRAME_START_IDENTIFIER:
-                        print('Waiting for frame start')
-                        header = sock.recv(32)
-                    n_packets, frame_id, time_started, server_time = struct.unpack('>BIdd', header[10:])
+                        #print('Waiting for frame start')
+                        header = sock.recv(40)
+                    n_packets, frame_id, time_started, server_time = struct.unpack('>IIdd', header[10:])
+                    #print(n_packets, frame_id, time_started, server_time)
                     buf = bytes()
                     check = True
                     for i in range(n_packets):
                         packet = sock.recv(1024)
-                        if packet[0] != i:
+                        if int.from_bytes(packet[0:4],'big') != i:
                             check = False
-                        buf += packet[1:]
+                        buf += packet[4:]
                     if check:
                         client_started = time.time()
                         setattr(TrafficMonitor(), 'cam%d' % self.camera_id, len(buf))
@@ -148,15 +168,19 @@ class FeedReceiver(threading.Thread):
                         img = QImage(img.tobytes('raw', 'RGB'), *img.size, QImage.Format_RGB888)
                         self.signals.imageReady.emit(img)
                         self.signals.updateStatus.emit(
-                                (time.time() - time_started) * 1000,
+                                (time.time() - time_started) * 1000,  # multiply by 1000 to cast to milliseconds
                                 server_time * 1000,
                                 (time.time() - client_started) * 1000,
                         )
                 except:
-                    raise
-        
-    def updateFrameSize(self,new_width):
-        self.width=new_width
+                    print(traceback.format_exc(),file=sys.stderr)
+                    setattr(FrameDropMonitor(), 'cam%d' % self.camera_id, 1)
+                    
+                
+    
+    def updateFrameSize(self, new_width):
+        self.width = new_width
+
 
 class CameraFeed(QWidget):
     def __init__(self, id, *args, **kwargs):
@@ -165,15 +189,17 @@ class CameraFeed(QWidget):
         self.box = QHBoxLayout()
         self.setLayout(self.box)
         self.video_frame = QLabel()
-        #self.video_frame.setScaledContents(True)
+        self.video_frame.setScaledContents(True)
         self.video_frame.setMinimumSize(1, 1)
-        self.video_frame.setSizePolicy(QSizePolicy.Expanding,QSizePolicy.Expanding)
+        self.video_frame.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.box.addWidget(self.video_frame)
         self.box.setContentsMargins(0, 0, 0, 0)
         self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
     
     def startReceiving(self):
         self.feed_receiver = FeedReceiver(self, self.id)
+        self.setVideoFramePlaceHolder()
         self.feed_receiver.signals.imageReady.connect(self.updateImage)
         self.feed_receiver.start()
     
@@ -183,14 +209,42 @@ class CameraFeed(QWidget):
         self.video_frame.setPixmap(img)
     
     def setVideoFramePlaceHolder(self):
-        img=QImage(self.feed_receiver.width, self.feed_receiver.width // 16 * 9, QImage.Format_Grayscale8)
+        img = QImage(self.feed_receiver.width, self.feed_receiver.width // 16 * 9, QImage.Format_Grayscale8)
         img.fill(2)
         self.updateImage(img)
-        
-    def resizeEvent(self, event):
-        self.feed_receiver.signals.frameResize.emit(self.frameGeometry().width())
-        self.setVideoFramePlaceHolder()
-        event.accept()
+    
+    # def resizeEvent(self, event):
+    #     self.feed_receiver.signals.frameResize.emit(self.frameGeometry().width())
+    #     #self.setVideoFramePlaceHolder()
+    #     event.accept()
+
+
+class Configuration(metaclass=SingletonMeta):
+    def __init__(self, n_camera):
+        self.n_camera = n_camera
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.lock = threading.Lock()
+        self.lock.acquire()
+        self.sock.connect((REMOTE_IP_ADDR, 5800))
+        self.lock.release()
+        self.configs = {'cam%d' % i: {'resolution': DEFAULT_RESOLUTION, 'quality': DEFAULT_IMAGE_QUALITY} for i in range(n_camera)}
+    
+    def update_config(self, cam_num, resolution, quality):
+        print(f'Updating for {cam_num}, res: {resolution}, qual: {quality}')
+        try:
+            self.configs['cam%d' % cam_num] = {'resolution': resolution, 'quality': quality}
+            self.lock.acquire()
+            self.sock.send(json.dumps(self.configs).encode())
+        finally:
+            self.lock.release()
+    
+    def change_settings(self, cam_num, cam_res, cam_qual):
+        d = {'cam' + str(cam_num): {"cam_res": cam_res, "cam_qual": cam_qual}}
+        self.sock.sendall(json.dumps(d).encode())
+    
+    def close(self):
+        self.sock.close()
+
 
 class CameraFeedEnclosing(QWidget):
     def __init__(self, id, *args, **kwargs):
@@ -212,7 +266,7 @@ class CameraFeedEnclosing(QWidget):
         
         self.tabs.addTab(self.camera_feed, 'Camera')
         self.tabs.addTab(self.status, 'Status')
-
+        
         self.camera_feed.feed_receiver.signals.updateStatus.connect(self.updateStatus)
     
     def initStatus(self):
@@ -235,6 +289,9 @@ class CameraFeedEnclosing(QWidget):
         self.traffic = QLabel()
         self.traffic.setText('0.000 KB/s')
         
+        self.frame_drop=QLabel()
+        self.frame_drop.setText('0 FPS')
+        
         # self.status_layout.addWidget(self.fps, 0, 0)
         
         self.status_layout.addWidget(QLabel("Traffic"), 0, 0)
@@ -254,24 +311,39 @@ class CameraFeedEnclosing(QWidget):
         
         self.status_layout.addWidget(QLabel("Frame Rate"), 5, 0)
         self.status_layout.addWidget(self.fps, 5, 1)
-
-        self.quality_slider = QSlider(Qt.Horizontal, )
+        
+        self.status_layout.addWidget(QLabel("Frame Drop"),6,0)
+        self.status_layout.addWidget(self.frame_drop,6,1)
+        
+        self.quality_slider = QSlider(Qt.Horizontal)
         self.quality_slider.setMinimum(1)
-        self.quality_slider.setMaximum(100)
+        self.quality_slider.setMaximum(80)
         self.quality_slider.setSingleStep(1)
         self.quality_slider.setValue(DEFAULT_IMAGE_QUALITY)
-    
+        
+        self.quality_label = QLabel(str(DEFAULT_IMAGE_QUALITY))
+        self.quality_slider.valueChanged.connect(lambda n: self.quality_label.setText(str(n)))
+        
         self.resolution_slider = QSlider(Qt.Horizontal, )
         self.resolution_slider.setMinimum(120)
         self.resolution_slider.setMaximum(1080)
         self.resolution_slider.setSingleStep(120)
         self.resolution_slider.setValue(DEFAULT_RESOLUTION)
         
-        self.status_layout.addWidget(QLabel("Image Quality"),6,0,columnspan=2)
-        self.status_layout.addWidget(self.quality_slider,7,0)
+        self.resolution_label = QLabel(str(DEFAULT_RESOLUTION))
+        self.resolution_slider.valueChanged.connect(lambda n: self.resolution_label.setText(str(n)))
         
-        self.status_layout.addWidget(QLabel("Image Resolution"),8,0,columnspan=2)
-        self.status_layout.addWidget(self.resolution_slider,9,0)
+        self.status_layout.addWidget(QLabel("Image Quality"), 7, 0, columnspan=2)
+        self.status_layout.addWidget(self.quality_slider, 8, 0)
+        self.status_layout.addWidget(self.quality_label, 8, 1)
+        
+        self.status_layout.addWidget(QLabel("Image Resolution"), 9, 0, columnspan=2)
+        self.status_layout.addWidget(self.resolution_slider, 10, 0)
+        self.status_layout.addWidget(self.resolution_label, 10, 1)
+        
+        self.apply_button = QPushButton("Apply")
+        self.status_layout.addWidget(self.apply_button,11,1)
+        self.apply_button.clicked.connect( lambda e: Configuration().update_config(self.id,self.resolution_slider.value(),self.quality_slider.value()) )
     
     def updateStatus(self, total, server, client):
         self.totalTime.setText('{: <4} ms'.format(str(total)))
@@ -280,6 +352,7 @@ class CameraFeedEnclosing(QWidget):
         self.networkTime.setText('{: <4} ms'.format(str(total - server - client)))
         self.traffic.setText('{: <4} KB/s'.format(str(round(getattr(TrafficMonitor(), 'cam%d' % self.id) / 1024, 1))))
         self.fps.setText('{: <4} FPS'.format(str(round(getattr(FrameRateMonitor(), 'cam%d' % self.id), 1))))
+        self.frame_drop.setText('{: <4} FPS'.format(str(round(getattr(FrameDropMonitor(), 'cam%d' % self.id), 1))))
 
 
 if __name__ == '__main__':
@@ -290,11 +363,14 @@ if __name__ == '__main__':
     app = QApplication([])
     
     app.setStyle('Fusion')
-    TrafficMonitor(4)
-    FrameRateMonitor(4)
-    
+    TrafficMonitor(1)
+    FrameRateMonitor(1)
+    FrameDropMonitor(1)
     en = CameraFeedEnclosing(0)
     
     en.show()
-    
-    app.exec_()
+    try:
+        Configuration(1)
+        app.exec_()
+    finally:
+        Configuration().close()
