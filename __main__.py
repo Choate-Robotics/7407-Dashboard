47c71d1ae68ee9dac26c7c83d59104725e522ed3
Annabelle Strong <annabellestrong@gmail.com>
2019-01-28 09:56:51 -0500

    Mockup folder and tab testing

--- __main__.py
+++ __main__.py
@@ -2,19 +2,19 @@ import os, sys
 sys.path.append(os.path.join(os.path.dirname(__file__),'..'))
 
 from PySide2.QtWidgets import QApplication
-from dashboard import Simulator
+from dashboard_2 import Simulator
 
 if __name__=='__main__':
     app = QApplication([])
     app.setStyle('Fusion')
-    
+
     sim = Simulator()
     sim.setWindowTitle('7407 Dashboard')
     sim.showFullScreen()
-    
+
     sys.stdout = sim.stdout
     sys.stderr = sim.stderr
-    
+
     sim.show()
     status = app.exec_()
     exit(status)
